54bbe2b9120166b1ad803f6cf68ad87d6f6ef39f
lipeng <lipeng@daokoudai.com>
2019-01-25 16:46:55 +0800

    v0.0.4 问卷页

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,60 +1,68 @@
-<template lang='pug'>
-    .container
-      .login
+<template lang="pug">
+.container
+    .login
         img(src='/static/imgs/guide_bg.jpg')
-      .learn
+    .learn
         button(
-          open-type="getUserInfo"
-          @getuserinfo="getUserInfo"
+            open-type="getUserInfo"
+            @getuserinfo="getUserInfo"
         ) 调查问卷
 </template>
 
 <script>
 export default {
-  methods: {
-    getUserInfo(e) {
-      // 判断授权是否成功
-      if(e.mp.detail.userInfo){
-        // console.log(e.mp.detail.userInfo)
-        // 存储到vuex
-        this.$store.dispatch('setIsAuthenticated',true)
-        this.$store.dispatch('setUser', e.mp.detail.userInfo)
-      }  
+    methods: {
+        getUserInfo(e) {
+            // 判断授权是否成功
+            if (e.mp.detail.userInfo) {
+                // console.log(e.mp.detail.userInfo)
+                // 存储到vuex
+                this.$store.dispatch('setIsAuthenticated', true)
+                this.$store.dispatch('setUser', e.mp.detail.userInfo)
+
+                wx.redirectTo({
+                    url: '../question/main',
+                })
+            }
+        },
     },
-  },
 }
 </script>
 
-<style scoped lang='scss'>
+<style lang="scss" scoped>
 .container {
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
 }
+
 .login {
-  width: 100%;
-  height: 100%;
-  position: relative;
+    width: 100%;
+    height: 100%;
+    position: relative;
 }
+
 .login img {
-  display: inline-block;
-  width: 100%;
-  height: 100%;
+    display: inline-block;
+    width: 100%;
+    height: 100%;
 }
+
 .login .learn {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  top: 0;
-  left: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    top: 0;
+    left: 0;
 }
+
 .learn button {
-  position: absolute;
-  top: 90%;
-  left: 10%;
-  width: 80%;
-  background-color: #009eef;
-  color: white;
+    position: absolute;
+    top: 90%;
+    left: 10%;
+    width: 80%;
+    background-color: #009eef;
+    color: white;
 }
 </style>
---
+++ src/pages/question/index.vue
@@ -0,0 +1,224 @@
+<template lang="pug">
+.question
+    img.bg(src='/static/imgs/test.jpg')
+    .qs_content(v-if='questions.length > 0')
+        div(v-if='!startqs')
+            p.title 温馨提示
+            .warn_tag 为了更好对的制定工作计划，提高协作效率，请首先回答以下问题
+        div(v-else)
+            p.title {{questions[currentIndex].title}}
+            .response(
+                v-for="(item,index) in questions[currentIndex].option" 
+                :key='index'
+                @click="selectAnswer(index)"
+                )
+                img(v-if='item.select' src='/static/imgs/selected.jpg')
+                img(v-else src='/static/imgs/unselect.jpg')
+                span {{item.label}}
+    button.qs_btn(
+        @click='btn_click'
+        :disabled="disabled"
+    ) {{btn_title}}
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            questions: [],
+            startqs: false, // 是否显示问题列表
+            currentIndex: 0, // 记录当前回答到第几道题
+            btn_title: '开始问答', // 按钮的title
+            lesson: '', // 记录提交的问题
+        }
+    },
+    onLoad() {
+        this.getData();
+    },
+    computed: {
+        disabled() {
+            if (!this.startqs) return false
+            else {
+                const option = this.questions[this.currentIndex].option
+                let select = false
+
+                option.forEach(item => {
+                    if (item.select) select = true
+                })
+
+                return !select
+            }
+        }
+    },
+    methods: {
+        getData() {
+            this.questions = [{
+                    "title": "是否赞成召开技术晨会？",
+                    "option": [{
+                            "label": "赞成",
+                            "select": false
+                        },
+                        {
+                            "label": "不赞成",
+                            "select": false
+                        }
+                    ]
+                },
+                {
+                    "title": "晨会合理开始的时间？",
+                    "option": [{
+                            "label": "九点半",
+                            "select": false
+                        },
+                        {
+                            "label": "十点",
+                            "select": false
+                        },
+                        {
+                            "label": "十点半",
+                            "select": false
+                        }
+                    ]
+                },
+                {
+                    "title": "所属技术开发组",
+                    "option": [{
+                            "label": "前端",
+                            "select": false
+                        },
+                        {
+                            "label": "后端",
+                            "select": false
+                        },
+                        {
+                            "label": "app",
+                            "select": false
+                        },
+                        {
+                            "label": "测试",
+                            "select": false
+                        }
+                    ]
+                },
+                {
+                    "title": "是否匿名？",
+                    "option": [{
+                            "label": "是",
+                            "select": false
+                        },
+                        {
+                            "label": "否",
+                            "select": false
+                        }
+                    ]
+                }
+            ]
+        },
+        btn_click() {
+            // 开始答题
+            if (!this.startqs) {
+                this.startqs = true
+                this.btn_title = '下一题'
+            }
+            // 答题中
+            else {
+                this.lesson += this.getSelectOption()
+                if (this.currentIndex < this.questions.length - 1) {
+                    this.currentIndex++
+                    this.lesson += ','
+                    // 最后一题
+                    if (this.currentIndex == this.questions.length - 1) {
+                        this.btn_title = '完成'
+                    }
+                } else {
+                    // console.log(this.lesson)
+                    // 完成
+                    this.sendQuestions()
+                }
+            }
+        },
+        selectAnswer(index) {
+            const option = this.questions[this.currentIndex].option
+            // 将列表中的是否选项都置为未选中
+            option.forEach(item => {
+                item.select = false
+            });
+            // 将点击的选项选中
+            option[index].select = !option[index].select
+        },
+        getSelectOption() {
+            const option = this.questions[this.currentIndex].option
+            let label = ''
+            option.forEach(item => {
+                if (item.select) label = item.label
+            })
+            return label
+        },
+        sendQuestions() {
+            // console.log(this.lesson)
+            // 存储对应成员答题情况，
+
+            // 跳转结果展示页面
+            wx.navigateTo({
+                url: `../result/mian?lesson=${this.lesson}`,
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+.question {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    position: relative;
+}
+
+.question .bg {
+    width: 100%;
+    height: 100%;
+}
+
+.qs_content {
+    position: absolute;
+    width: 80%;
+    height: 50%;
+    background: #fff;
+    top: 36%;
+    left: 10%;
+    border-radius: 5px;
+    box-sizing: border-box;
+    padding: 10px;
+    color: rgb(13, 28, 51);
+}
+
+.qs_btn {
+    position: absolute;
+    top: 90%;
+    left: 10%;
+    width: 80%;
+    background-color: #009eef;
+    color: white;
+}
+
+.title {
+    font-weight: bold;
+    margin-bottom: 20px;
+    word-break: break-all;
+}
+
+.warn_tag {
+    padding: 40px 10px;
+}
+
+.response {
+    padding: 10px;
+}
+
+.response img {
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+}
+</style>
