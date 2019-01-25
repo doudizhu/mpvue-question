17a4f1cb690b73a51d02b3aeb514ca16e786d76c
lipeng <lipeng@daokoudai.com>
2019-01-25 13:45:08 +0800

    v0.0.2 首页搭建

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,9 @@
 <template lang='pug'>
-  div hello mpvue
+    .container
+      .login
+        img(src='/static/imgs/guide_bg.jpg')
+      .learn
+        button 调查问卷
 </template>
 
 <script>
@@ -9,5 +13,35 @@ export default {
 </script>
 
 <style scoped lang='scss'>
-
+.container {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.login {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+.login img {
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+}
+.login .learn {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  top: 0;
+  left: 0;
+}
+.learn button {
+  position: absolute;
+  top: 90%;
+  left: 10%;
+  width: 80%;
+  background-color: #009eef;
+  color: white;
+}
 </style>
